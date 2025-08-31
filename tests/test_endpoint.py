5443db4ba974267f4843431ef815042926f5fac1
araliakbari <araliakbari@noornet.net>
2025-08-31 06:54:41 +0330

    Router Endpoint Test

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, APIRouter, Depends
 from fastapi.testclient import TestClient
 
 from .dependencies import State, IGlobalService, INumberService, DirectNumber, GlobalDirectNumber, GetDirectNumber, SetGlobalUsualNumber, GeneratorDependencyType
@@ -12,11 +12,11 @@ def test_AppEndpoint(app: FastAPI, client: TestClient, state: State):
     @app.get('/test', dependencies=[IGlobalService, GlobalDirectNumber, Depends(SetGlobalUsualNumber)])  # pyright: ignore[reportArgumentType]
     async def endpoint(text: str, service: INumberService, generator: GeneratorDependencyType, number: DirectNumber, number2: int = Depends(GetDirectNumber)) -> dict[str, Any]: # pyright: ignore[reportUnusedFunction]
         return {
-            "txt": text,
-            "srv": service.GetNumber(),
-            "gnr": generator, 
-            "n1": number,
-            "n2": number2,
+            'txt': text,
+            'srv': service.GetNumber(),
+            'gnr': generator, 
+            'n1': number,
+            'n2': number2,
         }
     
     response = client.get('/test', params={'text': QUERY_TEXT})
@@ -31,3 +31,22 @@ def test_AppEndpoint(app: FastAPI, client: TestClient, state: State):
     assert state.GlobalUsualNumber == GLOBAL_USUAL_NUMBER  # Use FastAPI dependencies in POD alongside FastDI deps 
     assert state.GeneratorExitNumber == GENERATOR_EXIT_NUMBER  # Ensure that clean-up block of generator works
 
+
+# --- Router Endpoint Test ---
+def test_RouterEndpoint(app: FastAPI, router: APIRouter, client: TestClient, state: State):
+
+    @router.get('/test', dependencies=[IGlobalService])  # pyright: ignore[reportArgumentType]
+    def endpoint(text: str, service: INumberService) -> dict[str, Any]:  # pyright: ignore[reportUnusedFunction]
+        return {
+            'txt': text,
+            'srv': service.GetNumber()
+        }
+    
+    app.include_router(router)
+    
+    response = client.get('/test', params={'text': QUERY_TEXT})
+    data = response.json()
+    assert response.status_code == 200
+    assert data['txt'] == QUERY_TEXT
+    assert data['srv'] == SERVICE_NUMBER
+    assert state.GlobalServiceNumber == GLOBAL_SERVICE_NUMBER
