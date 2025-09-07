1f2be6a2a71cac97838a35073e71b3bf73fca550
araliakbari <araliakbari@noornet.net>
2025-09-07 15:13:48 +0330

    Test integrations (incomplete)

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -1,1 +1,14 @@
-#TODO
+from typing import Any
+
+from fastapi import FastAPI as _FastAPI, Depends
+from fastapi.testclient import TestClient
+
+from fastdi.integrations import FastAPI, APIRouter
+
+from .dependencies import State
+from .constants import QUERY_TEXT
+
+
+# --- Application instance test
+def test_app(state: State):
+    ...
